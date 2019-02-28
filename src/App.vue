162e6cc022924603263c2f10e8807dd0e01ee4df
Pierre Schweiger <schweiger.pierre@gmail.com>
2019-02-27 22:47:29 -0300

    Remove font style from the app component

--- src/App.vue
+++ src/App.vue
@@ -16,13 +16,6 @@ export default {
 
 <style lang="scss">
 @import "./assets/styles/global.scss";
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
 .page{
   display: flex;
