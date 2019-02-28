08f1ab6f0608d09c6d9336f653344131f39cf48b
Pierre Schweiger <schweiger.pierre@gmail.com>
2019-02-27 22:36:42 -0300

    Change linter rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,17 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
+  extends: ["plugin:vue/recommended", "standard"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "vue/max-attributes-per-line": ["error", {
+      "singleline": 4,
+      "multiline": {
+        "max": 1,
+        "allowFirstLine": false
+      }
+    }]
   },
   parserOptions: {
     parser: "babel-eslint"
