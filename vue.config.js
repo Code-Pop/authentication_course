22ee86da15f193ec7c2aa76778c8c422810e34d2
Pierre Schweiger <schweiger.pierre@gmail.com>
2019-02-27 22:37:58 -0300

    Extract global css to it's own file

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+const path = require("path");
+
+module.exports = {
+  pluginOptions: {
+    "style-resources-loader": {
+      preProcessor: "scss",
+      patterns: [path.resolve(__dirname, "./src/styles/global.scss")]
+    }
+  }
+};
