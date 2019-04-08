41603987fe82f8cc67f9ca62feb3b6b4db15ef1a
Adam Jahr <adamjahr@gmail.com>
2019-04-08 16:03:17 -0400

    Lesson 3 finish code

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Dashboard from './views/Dashboard.vue'
+import RegisterUser from './views/RegisterUser.vue'
 
 Vue.use(Router)
 
@@ -18,6 +19,11 @@ const router = new Router({
       path: '/dashboard',
       name: 'dashboard',
       component: Dashboard
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterUser
     }
   ]
 })
---
+++ src/views/RegisterUser.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <form @submit.prevent="register">
+      <label for="name">
+        Name:
+      </label>
+      <input v-model="name" type="text" name="name" value>
+
+      <label for="email">
+        Email:
+      </label>
+      <input v-model="email" type="email" name="email" value>
+
+      <label for="password">
+        Password:
+      </label>
+      <input v-model="password" type="password" name="password" value>
+
+      <button type="submit" name="button">
+        Register
+      </button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      name: '',
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    register () {
+      this.$store
+        .dispatch('register', {
+          name: this.name,
+          email: this.email,
+          password: this.password
+        })
+        .then(() => {
+          this.$router.push({ name: 'dashboard' })
+        })
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
