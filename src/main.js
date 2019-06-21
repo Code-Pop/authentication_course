33f1b7d80169c50ef9b7c5aa0d9f57cfe133e171
Adam Jahr <adamjahr@gmail.com>
2019-06-20 23:21:27 -0400

    Lesson 7

--- src/main.js
+++ src/main.js
@@ -2,11 +2,29 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './vuex/store'
+import axios from 'axios'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  created () {
+    const userString = localStorage.getItem('user')
+    if (userString) {
+      const userData = JSON.parse(userString)
+      this.$store.commit('SET_USER_DATA', userData)
+    }
+    axios.interceptors.response.use(
+      response => response,
+      error => {
+        if (error.response.status === 401) {
+          this.$router.push('/')
+          this.$store.dispatch('logout')
+        }
+        return Promise.reject(error)
+      }
+    )
+  },
   render: h => h(App)
 }).$mount('#app')
